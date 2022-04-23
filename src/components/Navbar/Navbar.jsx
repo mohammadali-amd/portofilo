952607aab394f37b47349420efc8acd0e3db1369
mohammadali-amd <amidi1380@gmail.com>
2022-04-23 21:47:06 +0430

    update navbar & fix bugs

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,9 +11,11 @@ const Navbar = () => {
 
   return (
     <nav className='app__navbar'>
-      <div className='app__navbar-logo'>
-        <img src={images.logo} alt="logo" />
+      
+      <div>
+        <img src={images.logo_text} alt="logo-name" style={{ height:30, paddingTop: 10 }} />
       </div>
+      
       <ul className='app__navbar-links'>
         {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
           <li className='app__flex p-text' key={`link-${item}`}>
@@ -42,6 +44,14 @@ const Navbar = () => {
           </motion.div>
         )}
       </div>
+
+      <div className='app__navbar-logo'>
+        <p>
+          وب استودیو
+        </p>
+        <img src={images.logo} alt="logo" />
+      </div>
+      
     </nav>
   )
 }
