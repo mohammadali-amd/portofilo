c58af55bf7819e413762520c98c7d33ed29a57fe
mohammadali-amd <amidi1380@gmail.com>
2022-04-23 22:34:43 +0430

    fix screen bug on phone

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -31,23 +31,14 @@ const Header = () => {
           ))}
         </motion.div>
 
-        <img src={images.person} alt="profile_bg" />
-
-        {/* <motion.div
+        { <motion.div
           whileInView={{ opacity: [0,1] }}
           transition= {{ duration: 0.5, delayChildren: 0.5 }}
           className= 'app__header-img'
         >
-          {/* <img src={images.person} alt="profile_bg" /> }
-          <motion.img 
-            whileInView={{ scale: [0, 1] }}
-            transition={{ duration: 1, ease: 'easeInOut' }}
-            src={images.header_pic}
-            alt="profile_circle"
-            className="overlay_circle"
-          />
+          { <img src={images.person} alt="profile_bg" /> }
         </motion.div> 
-        */}
+        }
         
         <motion.div
           whileInView={{ x: [-100,0], opacity: [0,1] }}
