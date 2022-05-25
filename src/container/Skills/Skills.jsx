8f02748cb9f2c08057d5bab57b90817b78a2ac1f
mohammadali-amd <amidi1380@gmail.com>
2022-05-25 23:52:16 +0430

    delete comments

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -3,7 +3,6 @@ import { motion } from 'framer-motion';
 import ReactTooltip from 'react-tooltip';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
-// import { urlFor, client } from '../../client';
 import { images } from '../../constants'
 import './Skills.scss';
 
@@ -26,21 +25,6 @@ const experiences = [
 ];
 
 const Skills = () => {
-  // const [experiences, setExperiences] = useState([]);
-  // const [skills, setSkills] = useState([]);
-
-  // useEffect(() => {
-  //   const query = '*[_type == "experiences"]';
-  //   const skillsQuery = '*[_type == "skills"]';
-
-  //   client.fetch(query).then((data) => {
-  //     setExperiences(data);
-  //   });
-
-  //   client.fetch(skillsQuery).then((data) => {
-  //     setSkills(data);
-  //   });
-  // }, []);
 
   return (
     <>
